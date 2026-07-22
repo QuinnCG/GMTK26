37b028050579f70d1eab1551e72ced8721ea4a9f
Quinton <quinncg.dev@gmail.com>
2026-07-22 15:48:22 -0400

    Start on missile system

---
+++ Assets/Source/MissileSystem/Missile.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace GMTK26.MissileSystem
+{
+	public class Missile : MonoBehaviour
+	{
+		public void Init(Vector2 direction)
+		{
+
+		}
+	}
+}
---
+++ Assets/Source/MissileSystem/MissileManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace GMTK26.MissileSystem
+{
+	public class MissileManager : MonoBehaviour
+	{
+		private static MissileManager _instance;
+
+		private void Awake()
+		{
+			_instance = this;
+		}
+
+		public static Missile Spawn(Vector2 origin, Vector2 direction, GameObject prefab)
+		{
+			var instance = Object.Instantiate(prefab, origin, Quaternion.identity, _instance.transform);
+			var missile = instance.GetComponent<Missile>();
+			missile.Init(direction);
+
+			return missile;
+		}
+	}
+}
