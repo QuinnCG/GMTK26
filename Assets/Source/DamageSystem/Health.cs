2bf71d2193d8c35d265b6f7cff3d9ebe8d081e7a
Quinton <quinncg.dev@gmail.com>
2026-07-22 15:35:37 -0400

    Damage system skeleton

---
+++ Assets/Source/DamageSystem/Health.cs
@@ -0,0 +1,35 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace GMTK26.DamageSystem
+{
+	public class Health : MonoBehaviour, IDamageable
+	{
+		[SerializeField, Tooltip("The default health this entity starts with.")]
+		private float Default = 100f;
+		[field: SerializeField]
+		public Team Team { get; private set; } = Team.Hostile;
+
+		[ShowInInspector, ReadOnly, HideInEditorMode]
+		public float Current { get; private set; }
+		[ShowInInspector, ReadOnly, HideInEditorMode]
+		public float Max { get; private set; }
+		/// <summary>
+		/// A 0-1 value calculated as current HP divided by max HP, where 0 is dead and 1 is 100%.
+		/// </summary>
+		public float Normalized => Current / Max;
+
+		public bool IsDead { get; private set; }
+
+		private void Awake()
+		{
+			Current = Max = Default;
+		}
+
+		public bool TryTakeDamage(float damage, Team sourceTeam, Vector2 knockback = default)
+		{
+			Debug.Log("Health system not implemented yet!");
+			return true;
+		}
+	}
+}
